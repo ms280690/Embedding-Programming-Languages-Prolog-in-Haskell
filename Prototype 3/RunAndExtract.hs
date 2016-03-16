eaa9627241c6a44163e34cdec24becf31126b987
ms280690 <ms280690@gmail.com>
2016-03-15 18:26:36 -0700

    Moved run and extraction functions to RunAndExtract.hs and adjusted imports in prototype 3.

---
+++ Prototype 3/RunAndExtract.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE  DeriveDataTypeable, 
+        ViewPatterns, 
+        ScopedTypeVariables, 
+        FlexibleInstances, 
+        DefaultSignatures,
+        TypeOperators,
+        FlexibleContexts,
+        TypeFamilies,
+        DataKinds,
+        OverlappingInstances,
+        DataKinds,
+        PolyKinds,
+        TypeOperators,
+        LiberalTypeSynonyms,
+        TemplateHaskell,
+        RankNTypes,
+        AllowAmbiguousTypes
+        #-}
+
+{-# OPTIONS_HADDOCK show-extensions, ignore-exports #-}
+
+module RunAndExtract where
+
+import Data.Generics (Data(..), Typeable(..)) 
+
+import Data.Functor.Fixedpoint as DFF
+
+import Data.Traversable as T
+
+import Data.Foldable as DF
+
+import Control.Applicative ((<$>),(<*>),pure,Applicative)
+
+import Data.List.Extras.Pair
+
+import Control.Unification as U
+
+import Control.Unification.Types as UT
+
+import Control.Unification.STVar as ST 
+
+import qualified Data.Set as S
+
+import Data.Map as Map
+
+import Control.Monad.Trans.Except
+
+import Control.Monad.Error
+
+import Prolog
+
+import PrologFlat
+
+import Translators
+
+import VariableHandler
+
+instance (UT.Variable v, Functor t) => Error (UT.UFailure t v) where {}
+
+monadicUnification :: (BindingMonad FTS (STVar s FTS) (ST.STBinding s)) => (forall s. (Term -> Term -> ErrorT (UT.UFailure (FTS) (ST.STVar s (FTS)))
+           (ST.STBinding s) (UT.UTerm (FTS) (ST.STVar s (FTS)),
+            Map Id (ST.STVar s (FTS)))))
+monadicUnification t1 t2 = do
+  let
+    t1f = termFlattener t1
+    t2f = termFlattener t2
+  (x1,d1) <- lift . translateToUTerm $ t1f
+  (x2,d2) <- lift . translateToUTerm $ t2f
+  x3 <- U.unify x1 x2
+  --get state from somehwere, state -> dict
+  return $! (x3, d1 `Map.union` d2)
+
+
+runUnify ::
+  (forall s. (BindingMonad FTS (STVar s FTS) (ST.STBinding s))
+  =>
+      (ErrorT
+          (UT.UFailure FTS (ST.STVar s FTS))
+          (ST.STBinding s)
+          (UT.UTerm FTS (ST.STVar s FTS),
+             Map Id (ST.STVar s FTS)))
+     )
+  -> [(Id, Prolog)]
+runUnify test = ST.runSTBinding $ do
+  answer <- runErrorT $ test --ERROR
+  case answer of
+    (Left _)            -> return []
+    (Right (_, dict))   -> extractUnifier dict
+
+
+extractUnifier ::
+  (BindingMonad FTS (STVar s FTS) (ST.STBinding s))
+  => (forall s. Map Id (STVar s FTS)
+      -> (ST.STBinding s [(Id, Prolog)])
+     )
+extractUnifier dict = do
+  let ld1 = Map.toList dict
+  ld2 <- Control.Monad.Error.sequence [ v1 | (k,v) <- ld1, let v1 = UT.lookupVar v]
+  let ld3 = [ (k,v) | ((k,_),Just v) <- ld1 `zip` ld2]
+      ld4 = [ (k,v) | (k,v2) <- ld3, let v = translateFromUTerm dict v2 ]
+  return ld4
+
--- Prototype 3/Subst.hs
+++ Prototype 3/Subst.hs
@@ -36,6 +36,7 @@ import PrologFlat
 import VariableHandler
 import Translators
 import TestFunctions
+import RunAndExtract
 
 import Data.Map as Map
 import Data.Maybe
@@ -129,52 +130,52 @@ Var :: Term
 s1 @@ s2                 = app s1 . s2
 
 
-monadicUnification :: (BindingMonad FTS (STVar s FTS) (ST.STBinding s)) => (forall s. (Term -> Term -> ErrorT (UT.UFailure (FTS) (ST.STVar s (FTS)))
-           (ST.STBinding s) (UT.UTerm (FTS) (ST.STVar s (FTS)),
-            Map Id (ST.STVar s (FTS)))))
-monadicUnification t1 t2 = do
-  let
-    t1f = termFlattener t1
-    t2f = termFlattener t2
-  (x1,d1) <- lift . translateToUTerm $ t1f
-  (x2,d2) <- lift . translateToUTerm $ t2f
-  x3 <- U.unify x1 x2
-  --get state from somehwere, state -> dict
-  return $! (x3, d1 `Map.union` d2)
+-- monadicUnification :: (BindingMonad FTS (STVar s FTS) (ST.STBinding s)) => (forall s. (Term -> Term -> ErrorT (UT.UFailure (FTS) (ST.STVar s (FTS)))
+--            (ST.STBinding s) (UT.UTerm (FTS) (ST.STVar s (FTS)),
+--             Map Id (ST.STVar s (FTS)))))
+-- monadicUnification t1 t2 = do
+--   let
+--     t1f = termFlattener t1
+--     t2f = termFlattener t2
+--   (x1,d1) <- lift . translateToUTerm $ t1f
+--   (x2,d2) <- lift . translateToUTerm $ t2f
+--   x3 <- U.unify x1 x2
+--   --get state from somehwere, state -> dict
+--   return $! (x3, d1 `Map.union` d2)
 
 
-goUnify ::
-  (forall s. (BindingMonad FTS (STVar s FTS) (ST.STBinding s))
-  =>
-      (ErrorT
-          (UT.UFailure FTS (ST.STVar s FTS))
-          (ST.STBinding s)
-          (UT.UTerm FTS (ST.STVar s FTS),
-             Map Id (ST.STVar s FTS)))
-     )
-  -> [(Id, Prolog)]
-goUnify test = ST.runSTBinding $ do
-  answer <- runErrorT $ test --ERROR
-  case answer of
-    (Left _)            -> return []
-    (Right (_, dict))   -> f1 dict
+-- goUnify ::
+--   (forall s. (BindingMonad FTS (STVar s FTS) (ST.STBinding s))
+--   =>
+--       (ErrorT
+--           (UT.UFailure FTS (ST.STVar s FTS))
+--           (ST.STBinding s)
+--           (UT.UTerm FTS (ST.STVar s FTS),
+--              Map Id (ST.STVar s FTS)))
+--      )
+--   -> [(Id, Prolog)]
+-- goUnify test = ST.runSTBinding $ do
+--   answer <- runErrorT $ test --ERROR
+--   case answer of
+--     (Left _)            -> return []
+--     (Right (_, dict))   -> f1 dict
 
 
-f1 ::
-  (BindingMonad FTS (STVar s FTS) (ST.STBinding s))
-  => (forall s. Map Id (STVar s FTS)
-      -> (ST.STBinding s [(Id, Prolog)])
-     )
-f1 dict = do
-  let ld1 = Map.toList dict
-  ld2 <- sequence [ v1 | (k,v) <- ld1, let v1 = UT.lookupVar v]
-  let ld3 = [ (k,v) | ((k,_),Just v) <- ld1 `zip` ld2]
-      ld4 = [ (k,v) | (k,v2) <- ld3, let v = translateFromUTerm dict v2 ]
-  return ld4
+-- f1 ::
+--   (BindingMonad FTS (STVar s FTS) (ST.STBinding s))
+--   => (forall s. Map Id (STVar s FTS)
+--       -> (ST.STBinding s [(Id, Prolog)])
+--      )
+-- f1 dict = do
+--   let ld1 = Map.toList dict
+--   ld2 <- sequence [ v1 | (k,v) <- ld1, let v1 = UT.lookupVar v]
+--   let ld3 = [ (k,v) | ((k,_),Just v) <- ld1 `zip` ld2]
+--       ld4 = [ (k,v) | (k,v2) <- ld3, let v = translateFromUTerm dict v2 ]
+--   return ld4
 
 
 --unify :: Term -> Term -> [Subst]
-unify t1 t2 = substConvertor (goUnify (monadicUnification t1 t2))
+unify t1 t2 = substConvertor (runUnify (monadicUnification t1 t2))
 
 
 varX :: Term
